0dd53a5772c7aa2af6c870bd08eddef9efb43a85
Valeriia Tulchinskaia <72628164+valeri-tuly@users.noreply.github.com>
2026-03-29 19:11:17 +0300

    Add contact form handling with EmailJS integration

---
+++ js/contact.js
@@ -0,0 +1,29 @@
+// Инициализация EmailJS
+(function(){
+  emailjs.init("MeG_AUJX35hyw0i2i");
+})();
+// Обработка формы
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("#contact-form");
+
+    if (!form) return;
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        emailjs.send("service_b3lr60p", "template_5qdozpm", {
+            name: this.name.value,
+            email: this.email.value,
+            message: this.message.value,
+            company: this.company.value
+        }).then(() => {
+
+            alert("Kiitos! Viestisi on lähetetty ❤️");
+            this.reset();
+
+        }, (error) => {
+            alert("Virhe! Yritä uudelleen 😢");
+            console.log(error);
+        });
+    });
+});
