d62f044fc700812b192a6a3ef2fe1edc2ea7ca35
Valeriia Tulchinskaia <72628164+valeri-tuly@users.noreply.github.com>
2026-03-30 10:46:08 +0300

    Integrate EmailJS for footer contact form

--- js/footer-contact.js
+++ js/footer-contact.js
@@ -1,19 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const footerForm = document.querySelector("#footer-contact-form");
+import emailjs from 'https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js';
 
+emailjs.init('MeG_AUJX35hyw0i2i');
+
+document.addEventListener("DOMContentLoaded", () => {
+    const footerForm = document.querySelector("#footer-contact-form");
     if (!footerForm) return;
 
-    footerForm.addEventListener("submit", function (e) {
+    footerForm.addEventListener("submit", function(e) {
         e.preventDefault();
 
-        emailjs.send("service_b3lr60p", "template_ke477j8", {
+        emailjs.send('service_b3lr60p', 'template_ke477j8', {
             email: this.email.value
-        }).then(() => {
-
+        })
+        .then(() => {
             alert("Kiitos! Otamme sinuun yhteyttä 👍");
             this.reset();
-
-        }, (error) => {
+        })
+        .catch((error) => {
             alert("Virhe! Yritä uudelleen 😢");
             console.log(error);
         });
